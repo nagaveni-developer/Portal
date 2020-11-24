abc28b59bbdc76e1d6bb35ebbc1431366547b2aa
kp-debug <keerthipriya1438@gmail.com>
2020-11-24 15:40:48 +0530

    db-change:)

--- files.sql
+++ files.sql
@@ -1,11 +1,7 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: sql203.epizy.com
--- Generation Time: Sep 07, 2020 at 02:34 AM
--- Server version: 5.6.48-88.0
--- PHP Version: 7.2.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +15,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `epiz_26633019_project`
+-- Database: `files`
 --
 
 -- --------------------------------------------------------
@@ -48,6 +44,36 @@ INSERT INTO `category` (`id`, `name`, `description`) VALUES
 (7, 'Soft Skills', 'All queries regarding soft skills'),
 (8, 'Placements', 'All queries regarding Placements'),
 (9, 'Others', 'Any Other Category...');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data`
+--
+
+CREATE TABLE `data` (
+  `rollno` varchar(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `year` int(20) NOT NULL,
+  `branch` varchar(20) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `doc_req` varchar(20) NOT NULL,
+  `file` blob DEFAULT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp(),
+  `flag` int(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `data`
+--
+
+INSERT INTO `data` (`rollno`, `name`, `year`, `branch`, `mail`, `doc_req`, `file`, `date`, `flag`) VALUES
+('keerthi', 'keerthi priya', 2, 'CSE', 'keerthipriya1438@gmail.com', '', 0x73756e7365742d6269726473312e6a7067, '2020-11-24 10:27:48', 0),
+('keerthi', 'keerthi priya', 2, 'CSE', 'keerthipriya1438@gmail.com', 'renewal', 0x6f72616e67652d73756e7365742e6a7067, '2020-11-24 10:58:39', 0),
+('keerthi', 'keerthi priya', 2, 'CSE', 'keerthipriya1438@gmail.com', 'renewal', 0x6f72616e67652d73756e7365742e6a7067, '2020-11-24 11:02:55', 0),
+('keerthi', 'keerthi priya', 2, 'CSE', 'keerthipriya1438@gmail.com', 'renewal', 0x6f72616e67652d73756e7365742e6a7067, '2020-11-24 11:07:22', 0),
+('keerthi', 'keerthi priya', 2, 'CSE', 'keerthipriya1438@gmail.com', '', 0x6f72616e67652d73756e7365742e6a7067, '2020-11-24 11:08:08', 0),
+('priya1', 'keerthi priya', 2, 'cse', 'keerthipriya1438@gmail.com', '', 0x326e642d73656d2d6d61726b2d73686565742e706466, '2020-11-24 15:20:06', 0);
 
 -- --------------------------------------------------------
 
@@ -117,18 +143,15 @@ INSERT INTO `quacat` (`id`, `category`) VALUES
 CREATE TABLE `quans` (
   `id` int(11) UNSIGNED NOT NULL,
   `question` mediumtext NOT NULL,
-  `answer` longtext,
+  `answer` longtext DEFAULT NULL,
   `askedby` varchar(255) NOT NULL,
   `answeredby` varchar(255) DEFAULT NULL,
-  `likes` bigint(20) NOT NULL DEFAULT '0',
-  `dislikes` bigint(20) NOT NULL DEFAULT '0',
+  `likes` bigint(20) NOT NULL DEFAULT 0,
+  `dislikes` bigint(20) NOT NULL DEFAULT 0,
   `modified` datetime NOT NULL,
-  `status` tinyint(4) NOT NULL DEFAULT '1'
+  `status` tinyint(4) NOT NULL DEFAULT 1
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `quans`
---
 -- --------------------------------------------------------
 
 --
@@ -152,9 +175,13 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `year`, `branch`, `name`, `email`, `join_date`, `category`) VALUES
-(70, 'user', '$2y$10$b7RBlEP/HEv.k6A5hRlfpena00Rp6b3fRt4FhM0QcmIOyrzNeB01i', '3','cse','Nagaveni','chambetinagaveni1@gmail.com','2020-09-07',NULL),
 (17, 'admin', '$2y$10$P8b3vf6Z9U81CyDhoe4XhOglpC6BNPmpwoY46oxCyuXB4P0JDLDEG', '1', 'cse', 'Developer', 'vcequries82@gmail.com', '2017-04-11', NULL),
-(69, 'akhila_3103', '$2y$10$jR2KMRZbXMv50ePUXBWBje2aZQudpx94B3UgppJYcn1PqQwAdQKPi', '2nd', 'Cse', 'Analdas Akhila', 'akhila.analdas03@gmail.com', '2020-09-07', 'CSE,IT,Soft Skills,Placement,');
+(74, 'priya082', '$2y$10$P3qyzdghcGgKNUAERxNqAO4b56DPWu2F7uJQj3j26.Gz5BKCOR2DC', '2', 'cse', 'keerthi priya', 'keerthipriya1438@gmail.com', '2020-11-24', NULL),
+(70, '1', '$2y$10$GCDKfZLnE2VGdLQ2PvhbO.BE/yTT1kZnQmVeSvsJBILjjBzSpFocS', '1', '1', '1', 'keerthipriya1438@gmail.com', '2020-10-11', NULL),
+(71, 'keerthipriya', '$2y$10$CsCe/TbE0CXKm3lXFBIa8ePY4iVBAvZ9/Dw82w6W7qF.J4elDg8SS', '2', 'cse', 'keerthi priya', 'keerthipriya1438@gmail.com', '2020-10-27', NULL),
+(72, 'keerthi', '$2y$10$YSC8keKgyCinGUjYsPwaSO/ti9OUTH65up/tIuYE3dyR0ZmU9XqLS', '2', 'CSE', 'keerthi priya', 'keerthipriya1438@gmail.com', '2020-11-17', NULL),
+(73, 'priya', '$2y$10$EHDeyoBaM7sRXPXy8q4poehZpGWdFXKbEs5DKU8aJWnFeM8R31/86', '2', 'cse', 'priya', 'keerthipriyaa3@gmail.com', '2020-11-20', NULL),
+(75, 'priya1', '$2y$10$DJ8DtZWCJD6doMeyAodXeu8ZGUm.WLWedgfJBwlxW0SRTImONXnJ6', '2', 'cse', 'keerthi priya', 'keerthipriya1438@gmail.com', '2020-11-24', NULL);
 
 --
 -- Indexes for dumped tables
@@ -208,7 +235,7 @@ ALTER TABLE `quans`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
